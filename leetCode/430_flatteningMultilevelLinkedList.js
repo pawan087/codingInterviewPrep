8df3154f5a50f003ff6e31bf9ac4764e1a0ddaa9
Pawan <chahal.pawanpreet@gmail.com>
2021-12-27 14:13:22 -0800

    Added flatten doubly linked list question

---
+++ leetCode/430_flatteningMultilevelLinkedList.js
@@ -0,0 +1,88 @@
+// https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+
+class ListNode {
+  constructor(val, next = null, prev = null, child = null) {
+    this.val = val;
+    this.next = next;
+    this.prev = prev;
+    this.child = child;
+  }
+}
+
+const nodes = [1, [2, 7, [8, 10, 11], 9], 3, 4, [5, 12, 13], 6];
+
+const buildMultiLevel = function (nodes) {
+  const head = new ListNode(nodes[0]);
+  let currentNode = head;
+
+  for (let i = 1; i < nodes.length; i++) {
+    const val = nodes[i];
+    let nextNode;
+
+    if (Array.isArray(val)) {
+      nextNode = buildMultiLevel(val);
+      currentNode.child = nextNode;
+      continue;
+    }
+
+    nextNode = new ListNode(val);
+    currentNode.next = nextNode;
+    nextNode.prev = currentNode;
+    currentNode = nextNode;
+  }
+
+  return head;
+};
+
+let multiLinkedList = buildMultiLevel(nodes);
+// console.log(multiLinkedList);
+
+const printListMulti = (head) => {
+  if (!head) return;
+
+  console.log(head.val);
+
+  if (head.child) printListMulti(head.child);
+
+  printListMulti(head.next);
+};
+
+// printListMulti(multiLinkedList);
+
+function flatten(head) {
+  debugger;
+  let cur = head;
+
+  while (cur) {
+    if (cur.child) {
+      let last = flattenHelper(cur);
+
+      let nex = cur.next;
+      cur.next = cur.child;
+      cur.child.prev = cur;
+      cur.child = null;
+      last.next = nex;
+      
+      if (nex) {
+        nex.prev = last;
+      }
+    }
+
+    cur = cur.next;
+  }
+
+  return head;
+}
+
+function flattenHelper(node) {
+  let cur = node.child;
+
+  while (cur.next) {
+    cur = cur.next;
+  }
+
+  return cur;
+}
+
+debugger;
+console.log(flatten(multiLinkedList));
--- leetCode/92_reverseLinkedList2.js
+++ leetCode/92_reverseLinkedList2.js
@@ -98,42 +98,6 @@ const printList = (head) => {
   printList(head.next);
 };
 
-var reverseBetween = function (head, m, n) {
-  let currentPos = 1,
-    currentNode = head;
-  let start = head;
-
-  while (currentPos < m) {
-    start = currentNode;
-    currentNode = currentNode.next;
-    currentPos++;
-  }
-
-  let newList = null,
-    tail = currentNode;
-
-  while (currentPos >= m && currentPos <= n) {
-    const next = currentNode.next;
-    currentNode.next = newList;
-    newList = currentNode;
-    currentNode = next;
-    currentPos++;
-  }
-
-  start.next = newList;
-  tail.next = currentNode;
-
-  if (m > 1) {
-    return head;
-  } else {
-    return newList;
-  }
-};
-
-// printList(linkedList);
-// console.log("after reverse");
-// printList(reverseBetween(linkedList, 2, 4));
-
 /* ------------------------------------------------------ */
 
 function reverse(head, m, n) {
