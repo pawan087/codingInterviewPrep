c7f074a055974d677c8cc56907c7f3c300c0ba8c
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-01-06 11:29:58 -0800

    added leetcode 1376 timeNeededtoInformAllEmployees, graph pic along with brute force solution

---
+++ leetCode/1376_timeNeededtoInformAllEmployee.js
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/time-needed-to-inform-all-employees/
+
+/*
+
+8 employees: 0, 1, 2, 3, 4, 5, 6, 7
+
+headID = 4
+
+            0  1  2  3   4  5  6  7
+managers = [2, 2, 4, 6, -1, 4, 4, 5]
+
+see 1376_timeNeededToInformAllEmployees.jpg
+
+              0  1  2  3  4  5  6  7
+informTime = [0, 0, 4, 0, 7, 3, 6, 0]
+
+*/
+
+let n = 8;
+let headID = 4;
+let manager = [2, 2, 4, 6, -1, 4, 4, 5];
+let informTime = [0, 0, 4, 0, 7, 3, 6, 0];
+
+var numOfMinutes = function (n, headID, manager, informTime) {
+  let obj = {};
+
+  for (let i = 0; i < n; i++) {
+    obj[i] = new Array(0);
+  }
+
+  for (let i = 0; i < manager.length; i++) {
+    if (manager[i] !== -1) obj[manager[i]].push(i);
+  }
+
+  let maxSum = dfs(headID, obj, informTime);
+
+  return maxSum;
+};
+
+var dfs = function (idx, obj, informTime) {
+  if (obj[idx].length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  sum += informTime[idx];
+  let i = 0;
+
+  let arr = new Array(obj[idx].length).fill(0).map(() => new Array(0));
+
+  for (let employee of obj[idx]) {
+    sum += dfs(employee, obj, informTime);
+    arr[i].push(sum);
+    i++;
+    sum = informTime[idx];
+  }
+
+  let max = -Infinity;
+  arr.forEach((subArr) => (max = Math.max(subArr[0], max)));
+
+  return max;
+};
+
+console.log(numOfMinutes(n, headID, manager, informTime));
