40cf0789d0f34abfd14168998630570dd2848ce6
Pawan <chahal.pawanpreet@gmail.com>
2022-01-07 16:34:38 -0800

    Started leetCode 743

---
+++ leetCode/743_networkDelayTime.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/network-delay-time/
+
+let times1 = [
+  [2, 1, 1],
+  [2, 3, 1],
+  [3, 4, 1],
+];
+
+let n1 = 4;
+let k1 = 2;
+
+// => 2
+
+let times2 = [[1, 2, 1]];
+let n2 = 2;
+let k2 = 1;
+
+// => 1
+
+let times3 = [[1, 2, 1]];
+let n3 = 2;
+let k3 = 2;
+
+// => -1
+
+let times = [
+  [1, 2, 9],
+  [1, 4, 2],
+  [2, 5, 1],
+  [4, 2, 4],
+  [4, 5, 6],
+  [3, 2, 3],
+  [5, 3, 7],
+  [3, 1, 5],
+];
+
+let n = 5 // nodes = 1, 2, 3, 4, 5
+let k = 1;
+
