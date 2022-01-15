4070dae56ff4136d69dbdb8c3722a0c8c0069247
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-01-14 21:49:21 -0800

    added mochary interface design question, using enery tree

--- interfaceDesign/monarchy.js
+++ interfaceDesign/monarchy.js
@@ -1,18 +1,74 @@
 // see 'monarchy.jpg'
 
-new Monarchy("Jake");
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+    this.alive = true;
+  }
+}
 
-birth("Catherine", "Jake");
-birth("Jane", "Catherine");
-birth("Farah", "Jane");
-birth("Tom", "Jake");
-birth("Celine", "Jake");
-birth("Mark", "Catherine");
-birth("Peter", "Celine");
+class Monarchy {
+  constructor(king) {
+    this.king = new Person(king);
+    this.persons = {
+      [this.king.name]: this.king,
+    };
+  }
 
-getOrderOfSuccession(); // => ["Jake", "Catherine", "Jane", "Farah", "Mark", "Tom", "Celine", "Peter"];
+  birth(childName, parentName) {
+    let parent = this.persons[parentName];
+    let newChild = new Person(childName);
 
-death("Jake");
-death("Jane");
+    parent.children.push(newChild);
+    this.persons[childName] = newChild;
+  }
 
-getOrderOfSuccession(); // => ["Catherine", "Farah", "Mark", "Tom", "Celine", "Peter"];
+  death(name) {
+    let person = this.persons[name];
+
+    if (person === undefined) return `${name} does not exist`;
+
+    person.alive = false;
+  }
+
+  getOrderOfSuccession() {
+    let order = [];
+
+    this.dfs(this.king, order);
+
+    console.log(JSON.stringify(order));
+    return order;
+  }
+
+  dfs(curPerson, order) {
+    if (curPerson.alive === true) {
+      order.push(curPerson.name);
+    }
+
+    for (let child of curPerson.children) {
+      this.dfs(child, order);
+    }
+
+    return;
+  }
+}
+
+let x = new Monarchy("Jake");
+
+x.birth("Catherine", "Jake");
+x.birth("Jane", "Catherine");
+x.birth("Farah", "Jane");
+x.birth("Tom", "Jake");
+x.birth("Celine", "Jake");
+x.birth("Mark", "Catherine");
+x.birth("Peter", "Celine");
+
+console.log(x);
+
+x.getOrderOfSuccession(); // => ["Jake", "Catherine", "Jane", "Farah", "Mark", "Tom", "Celine", "Peter"];
+
+x.death("Jake");
+x.death("Jane");
+
+x.getOrderOfSuccession(); // => ["Catherine", "Farah", "Mark", "Tom", "Celine", "Peter"];
