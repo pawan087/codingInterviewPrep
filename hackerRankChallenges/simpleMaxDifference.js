690a7296b926f91e5f28434edbfbe0c656073706
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-01-20 09:40:25 -0800

    added brute force method for simpleMaxDif hackerrank challenge from aA

---
+++ hackerRankChallenges/simpleMaxDifference.js
@@ -0,0 +1,26 @@
+let input1 = [7, 1, 2, 5]; // => 4, 5 - 1 = 4
+let input2 = [7, 5, 3, 1]; // => -1, There's never a difference greater than zero
+
+function maxDifference(px) {
+  let maxDif = -Infinity;
+
+  for (let i = 0; i < px.length; i++) {
+    let curNum = px[i];
+
+    for (let j = 0; j < i; j++) {
+      let comparator = px[j];
+
+      if (comparator > curNum) {
+        continue;
+      }
+
+      let curDif = curNum - comparator;
+
+      maxDif = Math.max(curDif, maxDif);
+    }
+  }
+
+  return maxDif === -Infinity ? -1 : maxDif;
+}
+
+console.log(maxDifference(input1));
