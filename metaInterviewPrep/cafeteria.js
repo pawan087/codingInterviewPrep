3843723a970565f4454e52ae9b0aa131ec43d274
Pawan <chahal.pawanpreet@gmail.com>
2022-01-23 13:31:52 -0800

    added optimal solution (very difficult) (brute forcec much easier to follow)

--- metaInterviewPrep/cafeteria.js
+++ metaInterviewPrep/cafeteria.js
@@ -1,52 +1,46 @@
 // Cafeteria
 // https://www.facebookrecruiting.com/portal/coding_puzzles/?puzzle=203188678289677
 
-let N = 10;
-let K = 1;
-let M = 2;
-let S = [2, 6];
+// let N = 10;
+// let K = 1;
+// let M = 2;
+// let S = [2, 6];
+
+let N = 15
+let K = 2
+let M = 3
+let S = [11, 6, 14]
 
 // 1 2 3 4 5 6 7 8 9 10
 // [   ]   [   ]
 
+// [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
+//            *          *        *
+//        ^ ^   ^ ^ ^  ^    ^  ^     ^
+
 function getMaxAdditionalDinersCount(N, K, M, S) {
-    let count = 1;
-    let arr = new Array(N).fill(0).map(() => [count++, "Open"]);
-    let expandCount;
+  debugger;
+  let seat = 1;
+  let availableSeats = 0;
+  let delta = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-      let subArr = arr[i];
-      let num = subArr[0];
+  S.sort((a, b) => a - b);
+  // This trick is to count the available seats at the end if they exist
+  S.push(N + K + 1);
 
-      if (S.includes(num)) {
-        arr[i][1] = "Taken";
-      } else {
-        continue;
-      }
+  S.forEach((diner) => {
+    debugger;
+    delta = diner - K - seat;
+    availableSeats += delta > 0 ? Math.ceil(delta / (K + 1)) : 0;
+    seat = diner + K + 1;
+  });
 
-      expandCount = 0;
+  debugger;
+  return availableSeats;
+}
 
-      for (let j = i - 1; j >= 0; j--) {
-        arr[j][1] = "Closed";
-        expandCount++;
-
-        if (expandCount === K) break;
-      }
-
-      expandCount = 0;
-
-      for (let j = i + 1; j < arr.length; j++) {
-        arr[j][1] = "Closed";
-        expandCount++;
-
-        if (expandCount === K) break;
-      }
-    }
-
-    console.log(arr);
-
-    return;
-  }
+debugger;
+console.log(getMaxAdditionalDinersCount(N, K, M, S));
 
 // function getMaxAdditionalDinersCount(N, K, M, S) {
 //   let count = 1;
@@ -141,5 +135,3 @@ function getMaxAdditionalDinersCount(N, K, M, S) {
 
 //   return res;
 // }
-
-console.log(getMaxAdditionalDinersCount(N, K, M, S));
