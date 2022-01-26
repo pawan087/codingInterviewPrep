e1274c97fc2c438d09fff0293449d9c75b28d377
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-01-25 22:06:28 -0800

    added brute force mergeIntervals, doesn't pass

--- hackerRankChallenges/footballScores.js
+++ hackerRankChallenges/footballScores.js
@@ -1,6 +1,6 @@
 // Football Scores
 
-function counts(teamA, teamB) {
+function countsSlow(teamA, teamB) {
   let obj = {};
 
   for (let num of teamA) {
@@ -28,8 +28,43 @@ function counts(teamA, teamB) {
   return res;
 }
 
+function counts(teamA, teamB) {
+  teamB.sort((a, b) => a - b);
+  let b = 0;
+  teamA.sort((a, b) => a - b);
+  let count = 0;
+  let res = [];
+
+  for (let i = 0; i < teamA.length; i++) {
+    let numA = teamA[i];
+    let numB = teamB[b];
+
+    if (numA <= numB) {
+      count++;
+    }
+
+    if (numA > numB) {
+      b++;
+      res.push(count);
+      count++;
+    }
+
+    if (i === teamA.length - 1) {
+      res.push(count);
+    }
+  }
+
+  return res;
+}
+
 let teamA = [1, 4, 2, 4];
 let teamB = [3, 5];
 // => [2, 4]
 
 console.log(counts(teamA, teamB));
+
+/*
+
+1 2 4 4
+
+*/
--- hackerRankChallenges/mergeIntervals.js
+++ hackerRankChallenges/mergeIntervals.js
@@ -1,10 +1,53 @@
 // Merge Intervals
 
 function getMergedIntervals(intervals) {
-  return;
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let bool = false;
+  let min = Infinity;
+  let max = -Infinity;
+  let res = [];
+
+  for (let interval of intervals) {
+    let first = interval[0];
+    let second = interval[1];
+
+    if (first < min && bool === false) {
+      min = first;
+    }
+
+    if (second > max && bool === false) {
+      max = second;
+      bool = true;
+      continue;
+    }
+
+    if (bool === true && first > max) {
+      bool = false;
+      res.push([min, max]);
+      min = first;
+      max = second;
+      continue;
+    }
+
+    if (second > max && bool === true) {
+      max = second;
+      bool = false;
+      res.push([min, max]);
+      min = Infinity;
+      max = -Infinity;
+      continue;
+    }
+  }
+
+  if (!(min < res[res.length - 1][1])) {
+    res.push([min, max]);
+  }
+
+  return intervals;
 }
 
-let input = [
+let input1 = [
   [7, 7],
   [2, 3],
   [6, 11],
@@ -24,3 +67,5 @@ let input3 = [
   [2, 6],
   [5, 7],
 ]; // => [2, 8]
+
+console.log(getMergedIntervals(input2));
