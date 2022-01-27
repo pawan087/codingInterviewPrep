f55ed65442d88ec93d1d2105d38f7e88b2cc2727
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-01-27 11:29:21 -0800

    added leetcode 787, findCheapestPrice using Bellmon Ford

--- leetCode/787_cheapestFlightsWithinKStops.js
+++ leetCode/787_cheapestFlightsWithinKStops.js
@@ -2,18 +2,56 @@
 
 // https://leetcode.com/problems/cheapest-flights-within-k-stops/
 
+// function findCheapestPrice(n, flights, src, dst, k) {
+//   let costs = new Array(n).fill(Infinity);
+//   costs[src] = 0;
+
+//   let tmpCosts = costs.slice();
+
+//   for (let j = 0; j < k + 1; j++) {
+//     for (let i = 0; i < flights.length; i++) {
+//       let cur = flights[i];
+//       let curSrc = cur[0];
+//       let curDst = cur[1];
+//       let curCost = cur[2];
+
+//       if (costs[curDst] >= curCost + costs[curSrc]) {
+//         tmpCosts[curDst] = curCost + costs[curSrc];
+//       }
+//     }
+
+//     costs = tmpCosts.slice();
+//   }
+
+//   return costs[dst] === Infinity ? -1 : costs[dst];
+// }
+
 function findCheapestPrice(n, flights, src, dst, k) {
-  let obj = {};
+  let costs = new Array(n).fill(Infinity);
+  costs[src] = 0;
 
-  for (let i = 0; i < n; i++) {
-    obj[i] = [];
+  for (let i = 0; i < k + 1; i++) {
+    let tmpCosts = costs.slice();
+
+    for (let flight of flights) {
+      let s = flight[0];
+      let d = flight[1];
+      let c = flight[2];
+
+      if (costs[s] === Infinity) {
+        continue;
+      }
+
+      if (costs[s] + c < tmpCosts[d]) {
+        tmpCosts[d] = costs[s] + c;
+      }
+    }
+
+
+    costs = tmpCosts.slice();
   }
 
-  for (let arr of flights) {
-    obj[arr[0]].push([arr[1], arr[2]]);
-  }
-
-  return obj;
+  return costs[dst] === Infinity ? -1 : costs[dst];
 }
 
 let n = 3;
