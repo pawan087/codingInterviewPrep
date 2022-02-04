ac62922a92efe77f4bc76cfbcc097fcdc00c7b92
Pawan <chahal.pawanpreet@gmail.com>
2022-02-03 21:08:14 -0800

    added arrayMAnipulation

--- hackerRankChallenges/arrayManipulation.js
+++ hackerRankChallenges/arrayManipulation.js
@@ -17,33 +17,60 @@ let outputArr = [
   [03, 03, 03, 10, 10, 08, 08, 08, 01, 00],
 ];
 
+// function arrayManipulation(n, queries) {
+//   let grid = new Array(queries.length).fill(0).map(() => new Array(n).fill(00));
+//   let prev;
+
+//   for (let i = 0; i < grid.length; i++) {
+//     if (i > 0) {
+//       grid[i] = prev;
+//     }
+
+//     let low = queries[i][0] - 1;
+//     let high = queries[i][1] - 1;
+//     let curNum = queries[i][2];
+
+//     for (let j = 0; j < n; j++) {
+//       if (low <= j && j <= high) {
+//         grid[i][j] += curNum;
+//       }
+//     }
+
+//     prev = grid[i];
+//   }
+
+//   for (let arr of grid) {
+//     console.log(JSON.stringify(arr));
+//   }
+
+//   return Math.max(...grid[grid.length - 1]);
+// }
+
 function arrayManipulation(n, queries) {
-  let grid = new Array(queries.length).fill(0).map(() => new Array(n).fill(00));
-  let prev;
+  let arr = new Array(n).fill(0);
 
-  for (let i = 0; i < grid.length; i++) {
-    if (i > 0) {
-      grid[i] = prev;
+  for (let query of queries) {
+    let start = query[0];
+    let end = query[1];
+    let num = query[2];
+
+    arr[start - 1] += num;
+
+    if (end < arr.length) {
+      arr[end] += -num;
     }
-
-    let low = queries[i][0] - 1;
-    let high = queries[i][1] - 1;
-    let curNum = queries[i][2];
-
-    for (let j = 0; j < n; j++) {
-      if (low <= j && j <= high) {
-        grid[i][j] += curNum;
-      }
-    }
-
-    prev = grid[i];
   }
 
-  //   for (let arr of grid) {
-  // console.log(JSON.stringify(arr));
-  //   }
+  let max = -Infinity;
+  let sum = 0;
 
-  return Math.max(...grid[grid.length - 1]);
+  for (let num of arr) {
+    sum += num;
+
+    max = Math.max(max, sum);
+  }
+
+  return max;
 }
 
 console.log(arrayManipulation(n, input));
