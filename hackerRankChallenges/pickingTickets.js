381293178317fae8f8c41e4a159242297908d9f6
Pawan <chahal.pawanpreet@gmail.com>
2022-02-11 10:59:08 -0800

    added pickingTickets (all specs passing), can optimize by not creating extra space and keep count of longest sequence

--- hackerRankChallenges/pickingTickets.js
+++ hackerRankChallenges/pickingTickets.js
@@ -1,10 +1,30 @@
 // Picking Tickets
 
 function maxTickets(tickets) {
-  return;
+  let sorted = tickets.slice().sort((a, b) => a - b);
+  let arr = [];
+  let subArr = [];
+
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] === sorted[i + 1] || sorted[i] + 1 === sorted[i + 1]) {
+      subArr.push(sorted[i]);
+    } else {
+      subArr.push(sorted[i]);
+      arr.push(subArr);
+      subArr = [];
+    }
+  }
+
+  let res = 0;
+
+  for (let nums of arr) {
+    res = Math.max(res, nums.length);
+  }
+
+  return res;
 }
 
 let input = [8, 5, 4, 8, 4]; // 3
 let input2 = [4, 13, 2, 3]; // 3
 
-console.log(maxTickets(input));
+console.log(maxTickets(input2));
