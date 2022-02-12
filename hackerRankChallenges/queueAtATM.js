6208a1ca2a2255e42db1311ea4cbb2fd4e355277
Pawan <chahal.pawanpreet@gmail.com>
2022-02-11 16:36:01 -0800

    added optimal queueAtATM solution O(n) runtime

--- hackerRankChallenges/queueAtATM.js
+++ hackerRankChallenges/queueAtATM.js
@@ -1,30 +1,56 @@
 // Queue at ATM
 
+// function getFinalOrder(k, amount) {
+//   let obj = {};
+
+//   for (let i = 1; i <= amount.length; i++) {
+//     obj[i] = amount[i - 1];
+//   }
+
+//   let res = [];
+
+//   while (Object.keys(obj).length > 0) {
+//     for (let key in obj) {
+//       let val = obj[key];
+
+//       val = val - k;
+
+//       obj[key] = val;
+
+//       if (val <= 0) {
+//         res.push(Number(key));
+
+//         delete obj[key];
+//       }
+//     }
+//   }
+
+//   return res;
+// }
+
 function getFinalOrder(k, amount) {
-  let obj = {};
-
-  for (let i = 1; i <= amount.length; i++) {
-    obj[i] = amount[i - 1];
-  }
-
   let res = [];
+  let toDo = true;
 
-  while (Object.keys(obj).length > 0) {
-    for (let key in obj) {
-      let val = obj[key];
+  while (toDo === true) {
+    toDo = false;
 
-      val = val - k;
+    for (let i = 0; i < amount.length; i++) {
+      if (typeof amount[i] !== "number") {
+        continue;
+      }
 
-      obj[key] = val;
+      toDo = true;
 
-      if (val <= 0) {
-        res.push(Number(key));
+      amount[i] = amount[i] - k;
 
-        delete obj[key];
+      if (amount[i] <= 0) {
+        res.push(i + 1);
+        amount[i] = String(amount[i]);
       }
     }
   }
-
+  
   return res;
 }
 
@@ -34,10 +60,10 @@ let input = [2, 5, 1]; // [1, 3, 2]
 
 let k2 = 3;
 let n2 = 3;
-let input2 = [1, 3, 2]; // [1, 3, 2]
+let input2 = [1, 3, 2]; // [1, 2, 3]
 
 let k3 = 2;
 let n3 = 6;
 let input3 = [6, 1, 2, 3, 2, 7]; // [2, 3, 5, 4, 1, 6]
 
-console.log(getFinalOrder(k, input));
+console.log(getFinalOrder(k3, input3));
