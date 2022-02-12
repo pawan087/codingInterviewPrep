b9204f55280f71b01ea82064c925a8e096266cf6
Pawan <chahal.pawanpreet@gmail.com>
2022-02-11 20:49:45 -0800

    added starting of leetcode 46, also adding combinators

---
+++ leetCode/46_permutations.js
@@ -0,0 +1,29 @@
+// LeetCode 46 Permutations (Medium)
+
+var permute = function (nums) {
+  return;
+};
+
+// Example 1:
+let nums1 = [1, 2, 3];
+let output1 = [
+  [1, 2, 3],
+  [1, 3, 2],
+  [2, 1, 3],
+  [2, 3, 1],
+  [3, 1, 2],
+  [3, 2, 1],
+];
+
+// Example 2:
+let nums2 = [0, 1];
+let output2 = [
+  [0, 1],
+  [1, 0],
+];
+
+// Example 3:
+let nums3 = [1];
+let output3 = [[1]];
+
+console.log(permute(nums1));
