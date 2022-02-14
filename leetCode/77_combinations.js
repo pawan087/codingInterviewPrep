f49f05122d2f2b598e9ed819d02ed5c7653f26b9
Pawan <chahal.pawanpreet@gmail.com>
2022-02-13 17:33:46 -0800

    added recursive solution to combinations

--- leetCode/77_combinations.js
+++ leetCode/77_combinations.js
@@ -19,38 +19,71 @@ let k2 = 1;
 
 let output2 = [[1]];
 
+// let combine = function (n, k) {
+//   let count = 0;
+//   let arr = new Array(n).fill(0).map(() => ++count);
+//   let res = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let j = i + 1;
+//     let subArr = [];
+//     let len = arr.length - i;
+
+//     while (subArr.length !== len) {
+//       let subSubArr = [arr[i]];
+
+//       if (arr[j] === undefined) {
+//         break;
+//       }
+
+//       while (subSubArr.length !== k) {
+//         subSubArr.push(arr[j]);
+
+//         j++;
+//       }
+
+//       subArr.push(subSubArr);
+//     }
+
+//     if (subArr.length > 0) {
+//       res = [...res, ...subArr];
+//     }
+//   }
+
+//   return res;
+// };
+
 let combine = function (n, k) {
-  let count = 0;
-  let arr = new Array(n).fill(0).map(() => ++count);
+  // Instantiate results array
   let res = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let j = i + 1;
-    let subArr = [];
-    let len = arr.length - i;
-
-    while (subArr.length !== len) {
-      let subSubArr = [arr[i]];
-
-      if (arr[j] === undefined) {
-        break;
-      }
-
-      while (subSubArr.length !== k) {
-        subSubArr.push(arr[j]);
-
-        j++;
-      }
-
-      subArr.push(subSubArr);
+  // Helper function
+  let backtrack = function (start, arr) {
+    if (arr.length === k) {
+      // If the array in formation is the same length as k
+      // then stop adding to the array and place the array in formation in into results
+      res.push(arr.slice());
+      return;
     }
 
-    if (subArr.length > 0) {
-      res = [...res, ...subArr];
+    for (let i = start; i <= n; i++) {
+      // Start i at 'start' and iterate until it equals n
+      // Add into the array the current i
+      arr.push(i);
+
+      // Continue adding to array in formation starting with the next index
+      // Make sure to pass the array in formation as an arguement
+      backtrack(i + 1, arr);
+
+      // Remove the last added item and continue the iteration
+      arr.pop();
     }
-  }
+  };
+
+  // Add into results array by initiating function with start value of 1
+  backtrack(1, []);
 
   return res;
 };
 
-console.log(combine(n1, k1));
+console.log(combine(4, 3));
