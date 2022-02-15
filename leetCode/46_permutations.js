24878b83e5e84fdb99e57a3e3bc08606e84f907e
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-02-14 19:05:46 -0800

    added permutations 46

--- leetCode/46_permutations.js
+++ leetCode/46_permutations.js
@@ -1,5 +1,113 @@
 // LeetCode 46 Permutations (Medium)
 
+// let permute = (nums) => {
+//   debugger;
+//   let res = [];
+
+//   let helper = (cur, rest) => {
+//     debugger;
+//     if (rest.length === 0) {
+//       debugger;
+//       res.push(cur);
+
+//       return;
+//     }
+
+//     debugger;
+//     for (let i = 0; i < rest.length; i++) {
+//       debugger;
+//       let argOne = [...cur, rest[i]]; // Cur
+//       let argTwo = [...rest.slice(0, i), ...rest.slice(i + 1)]; // Rest
+
+//       debugger;
+//       helper(argOne, argTwo);
+
+//       debugger;
+//     }
+//   };
+
+//   debugger;
+//   helper([], nums);
+
+//   debugger;
+//   return res;
+// };
+
+// let permute2 = function (nums) {
+//   debugger;
+//   let result = [];
+//   let temp = [];
+
+//   debugger;
+//   findPermutations(temp, nums, result);
+
+//   debugger;
+//   return result;
+// };
+
+// let findPermutations = (temp, nums, result) => {
+//   debugger;
+//   if (nums.length === 0) {
+//     debugger;
+//     result.push(temp.slice());
+
+//     return;
+//   }
+
+//   debugger;
+//   for (let i = 0; i < nums.length; i++) {
+//     debugger;
+//     let newNum = nums[i];
+
+//     temp.push(newNum);
+//     nums.splice(i, 1);
+
+//     debugger;
+//     findPermutations(temp, nums, result);
+
+//     debugger;
+//     temp.pop();
+//     nums.splice(i, 0, newNum);
+//   }
+
+//   debugger;
+// };
+
+// let permute3 = function (nums) {
+//   debugger;
+//   let res = [];
+
+//   let helper = function (visited = new Set(), curPerm = []) {
+//     debugger;
+//     if (curPerm.length === nums.length) {
+//       debugger;
+//       res.push(curPerm);
+
+//       return;
+//     }
+
+//     debugger;
+//     for (let i = 0; i < nums.length; i++) {
+//       debugger;
+//       if (visited.has(i) === false) {
+//         debugger;
+//         let argOne = new Set([...visited, i]); // Visited
+//         let argTwo = [...curPerm, nums[i]]; // curPerm
+
+//         debugger;
+//         helper(argOne, argTwo);
+//         debugger;
+//       }
+//     }
+//   };
+
+//   debugger;
+//   helper();
+
+//   debugger;
+//   return res;
+// };
+
 let permute = (nums) => {
   let res = [];
 
@@ -23,62 +131,8 @@ let permute = (nums) => {
   return res;
 };
 
-let permute2 = function (nums) {
-  let result = [];
-  let temp = [];
-
-  findPermutations(temp, nums, result);
-
-  return result;
-};
-
-let findPermutations = (temp, nums, result) => {
-  if (nums.length === 0) {
-    result.push(temp.slice());
-
-    return;
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    let newNum = nums[i];
-
-    temp.push(newNum);
-    nums.splice(i, 1);
-
-    findPermutations(temp, nums, result);
-
-    temp.pop();
-    nums.splice(i, 0, newNum);
-  }
-};
-
-let permute3 = function (nums) {
-  let res = [];
-
-  let helper = function (visited = new Set(), curPerm = []) {
-    if (curPerm.length === nums.length) {
-      res.push(curPerm);
-
-      return;
-    }
-
-    for (let i = 0; i < nums.length; i++) {
-      if (visited.has(i) === false) {
-        let argOne = new Set([...visited, i]); // Visited
-        let argTwo = [...curPerm, nums[i]]; // curPerm
-
-        helper(argOne, argTwo);
-      }
-    }
-  };
-
-  helper();
-
-  return res;
-};
-
 // Example 1:
-let nums1 = [1, 2, 3];
+let nums1 = [1, 2, 3,4,5,6,7,8,9,10];
 let output1 = [
   [1, 2, 3],
   [1, 3, 2],
@@ -99,4 +153,5 @@ let output2 = [
 let nums3 = [1];
 let output3 = [[1]];
 
-console.log(permute3(nums1));
+// debugger;
+console.log(permute(nums1));
