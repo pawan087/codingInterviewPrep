26a024a95e6cee384b0d6bc079e60d9915c9d1a4
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-02-14 17:21:34 -0800

    added permutations solution and two alternates

--- leetCode/46_permutations.js
+++ leetCode/46_permutations.js
@@ -1,7 +1,80 @@
 // LeetCode 46 Permutations (Medium)
 
-var permute = function (nums) {
-  return;
+let permute = (nums) => {
+  let res = [];
+
+  let helper = (cur, rest) => {
+    if (rest.length === 0) {
+      res.push(cur);
+
+      return;
+    }
+
+    for (let i = 0; i < rest.length; i++) {
+      let argOne = [...cur, rest[i]]; // Cur
+      let argTwo = [...rest.slice(0, i), ...rest.slice(i + 1)]; // Rest
+
+      helper(argOne, argTwo);
+    }
+  };
+
+  helper([], nums);
+
+  return res;
+};
+
+let permute2 = function (nums) {
+  let result = [];
+  let temp = [];
+
+  findPermutations(temp, nums, result);
+
+  return result;
+};
+
+let findPermutations = (temp, nums, result) => {
+  if (nums.length === 0) {
+    result.push(temp.slice());
+
+    return;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let newNum = nums[i];
+
+    temp.push(newNum);
+    nums.splice(i, 1);
+
+    findPermutations(temp, nums, result);
+
+    temp.pop();
+    nums.splice(i, 0, newNum);
+  }
+};
+
+let permute3 = function (nums) {
+  let res = [];
+
+  let helper = function (visited = new Set(), curPerm = []) {
+    if (curPerm.length === nums.length) {
+      res.push(curPerm);
+
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (visited.has(i) === false) {
+        let argOne = new Set([...visited, i]); // Visited
+        let argTwo = [...curPerm, nums[i]]; // curPerm
+
+        helper(argOne, argTwo);
+      }
+    }
+  };
+
+  helper();
+
+  return res;
 };
 
 // Example 1:
@@ -26,4 +99,4 @@ let output2 = [
 let nums3 = [1];
 let output3 = [[1]];
 
-console.log(permute(nums1));
+console.log(permute3(nums1));
--- leetCode/77_combinations.js
+++ leetCode/77_combinations.js
@@ -19,47 +19,16 @@ let k2 = 1;
 
 let output2 = [[1]];
 
-// let combine = function (n, k) {
-//   let count = 0;
-//   let arr = new Array(n).fill(0).map(() => ++count);
-//   let res = [];
-
-//   for (let i = 0; i < arr.length; i++) {
-//     let j = i + 1;
-//     let subArr = [];
-//     let len = arr.length - i;
-
-//     while (subArr.length !== len) {
-//       let subSubArr = [arr[i]];
-
-//       if (arr[j] === undefined) {
-//         break;
-//       }
-
-//       while (subSubArr.length !== k) {
-//         subSubArr.push(arr[j]);
-
-//         j++;
-//       }
-
-//       subArr.push(subSubArr);
-//     }
-
-//     if (subArr.length > 0) {
-//       res = [...res, ...subArr];
-//     }
-//   }
-
-//   return res;
-// };
-
 let combineRecursive = function (n, k) {
+  // debugger;
   // Instantiate results array
   let res = [];
 
   // Helper function
   let backtrack = function (start, arr) {
+    // debugger;
     if (arr.length === k) {
+      // debugger;
       // If the array in formation is the same length as k
       // then stop adding to the array and place the array in formation in into results
       res.push(arr.slice());
@@ -67,18 +36,21 @@ let combineRecursive = function (n, k) {
     }
 
     for (let i = start; i <= n; i++) {
+      // debugger;
       // Start i at 'start' and iterate until it equals n
       // Add into the array the current i
       arr.push(i);
 
       // Continue adding to array in formation starting with the next index
-      // Make sure to pass the array in formation as an arguement
+      // Make sure to pass the array in formation as an argument
       backtrack(i + 1, arr);
 
       // Remove the last added item and continue the iteration
       arr.pop();
     }
   };
+
+  // debugger;
 
   // Add into results array by initiating function with start value of 1
   backtrack(1, []);
@@ -87,14 +59,14 @@ let combineRecursive = function (n, k) {
 };
 
 function combine(n, k) {
-  let frame = new Array(k).fill(0);
-  let ans = [];
+  let arr = new Array(k).fill(0);
+  let res = [];
   let i = 0;
 
   while (i >= 0) {
-    frame[i] += 1;
+    arr[i] += 1;
 
-    if (frame[i] === n + 1) {
+    if (arr[i] === n + 1) {
       i -= 1;
 
       continue;
@@ -104,14 +76,15 @@ function combine(n, k) {
 
     if (i === k) {
       i -= 1;
-      ans.push(frame.slice());
+      res.push(arr.slice());
       continue;
     }
 
-    frame[i] = frame[i - 1];
+    arr[i] = arr[i - 1];
   }
 
-  return ans;
+  return res;
 }
 
-console.log(combine(n1, 3));
+// debugger;
+console.log(combineRecursive(6, 3));
