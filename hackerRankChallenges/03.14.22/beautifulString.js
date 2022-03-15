50ba119f38f5ffd657ed92cd5ce3ee739e330ecc
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-03-14 20:17:51 -0700

    added beautifulString hackerRank challenege, not passing all specs

---
+++ hackerRankChallenges/03.14.22/beautifulString.js
@@ -0,0 +1,30 @@
+// Beautiful String
+
+function getMinimumOperationCount(s) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  let count = 0;
+
+  for (let i = 0; i < s.length; i += 2) {
+    let cur = s[i];
+    let nex = s[i + 1];
+
+    if (nex === undefined) {
+      continue;
+    }
+
+    let curIdx = alphabet.indexOf(cur);
+    let nexIdx = alphabet.indexOf(nex);
+
+    if (nexIdx - curIdx === 1 || nexIdx - curIdx === 0) {
+      count++;
+    }
+  }
+
+  console.log(s);
+
+  return count;
+}
+
+let input = "abdde"; // => 2
+
+console.log(getMinimumOperationCount(input));
