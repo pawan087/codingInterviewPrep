940197b0ab27aa0972a25346e7155d976415c3b3
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-03-15 20:58:51 -0700

    added firstUniqueCharacter hackerRank challenege, all specs passing

--- hackerRankChallenges/03.15.22/firstUniqueCharacter.js
+++ hackerRankChallenges/03.15.22/firstUniqueCharacter.js
@@ -1,2 +1,34 @@
 // First Unique Character
 
+function getUniqueCharacter(s) {
+  if (s.length === 0) {
+    return -1;
+  }
+  
+  let obj = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+
+    if (obj[char] === undefined) {
+      obj[char] = [i + 1, 1];
+    } else {
+      obj[char][1]++;
+    }
+  }
+
+  for (let key in obj) {
+    let val = obj[key];
+    let count = val[1];
+
+    if (count === 1) {
+      return val[0];
+    }
+  }
+
+  return -1;
+}
+
+let s = "statistics";
+
+console.log(getUniqueCharacter(s));
